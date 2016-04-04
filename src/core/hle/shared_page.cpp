287d34621d59159d00649d34ac174a9ded204dfa
Ryan Loebs <obsidianx@gmail.com>
2016-04-04 01:31:48 -0700

    Implement actual system time
    libctru: osGetTime();

--- src/core/hle/shared_page.cpp
+++ src/core/hle/shared_page.cpp
@@ -3,6 +3,7 @@
 // Refer to the license.txt file included.
 
 #include <cstring>
+#include <ctime>
 
 #include "core/hle/shared_page.h"
 
@@ -15,7 +16,16 @@ SharedPageDef shared_page;
 void Init() {
     std::memset(&shared_page, 0, sizeof(shared_page));
 
+    // 3DS uses 1/1/1900 for Epoch
+    time_t plat_time = time(NULL);
+    struct tm console_epoch = {0};
+    console_epoch.tm_mday = 1;
+    u64_le console_time = (u64_le) difftime(plat_time, mktime(&console_epoch)) * 1000L;
+
     shared_page.running_hw = 0x1; // product
+    shared_page.date_time_0 = {
+            .date_time = console_time,
+    };
 }
 
 } // namespace
